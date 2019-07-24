72164c3e739518937731f5f14395565c4f9a8848
Raul Metsma <raul@metsma.ee>
2019-07-24 14:44:02 +0300

    Remove idemiaawppkcs11.json on uninstall (#33)
    IB-5934
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- uninstall.sh
+++ uninstall.sh
@@ -16,6 +16,7 @@ function remove_all {
         /Library/Application\ Support/Google/Chrome/External\ Extensions/ckjefchnfjhjfedoccjbhjpbncimppeg.json \
         /Library/Application\ Support/Mozilla/NativeMessagingHosts/ee.ria.esteid.json \
         /Library/Application\ Support/Mozilla/PKCS11Modules/onepinopenscpkcs11.json \
+        /Library/Application\ Support/Mozilla/PKCS11Modules/idemiaawppkcs11.json \
         /Library/Application\ Support/Mozilla/Extensions/\{ec8030f7-c20a-464f-9b0e-13a3a9e97384\}/\{443830f0-1fff-4f9a-aa1e-444bafbc7319\}.xpi \
         /Library/Application\ Support/Mozilla/Extensions/\{ec8030f7-c20a-464f-9b0e-13a3a9e97384\}/\{02274e0c-d135-45f0-8a9c-32b35110e10d\}.xpi \
         /Applications/Utilities/TokenSigningSafariHelper.app
