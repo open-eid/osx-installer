000f1c7e73e5789954a52d09a16f13a27d648c29
Raul Metsma <raul@metsma.ee>
2020-04-15 09:37:04 +0300

    Remove firefox-token-signing (#42)
    IB-6209
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- uninstall.sh
+++ uninstall.sh
@@ -10,7 +10,7 @@ function remove_all {
     sudo rm -rf /Library/PreferencePanes/id-updater.prefPane
 
     echo 'Removing browser plug-ins and extensions'
-    sudo rm -rf /Library/Internet\ Plug-Ins/esteidfirefoxplugin.bundle \
+    sudo rm -rf \
         /Library/Google/Chrome/NativeMessagingHosts/chrome-token-signing.app \
         /Library/Google/Chrome/NativeMessagingHosts/ee.ria.esteid.json \
         /Library/Application\ Support/Google/Chrome/External\ Extensions/ckjefchnfjhjfedoccjbhjpbncimppeg.json \
@@ -40,7 +40,6 @@ function remove_all {
     sudo pkgutil --forget ee.ria.token-signing-chrome-policy
     sudo pkgutil --forget ee.ria.chrome-token-signing-policy
     sudo pkgutil --forget ee.ria.safari-token-signing
-    sudo pkgutil --forget ee.ria.firefox-token-signing
     sudo pkgutil --forget ee.ria.firefox-pkcs11-loader
     sudo pkgutil --forget ee.ria.esteid-tokend
     sudo pkgutil --forget ee.ria.esteid-ctk-tokend
