afa5016a044ee8a8c4cdf7b9406fda95ff259226
Raul Metsma <raul@metsma.ee>
2021-10-22 09:31:16 +0300

    Try to remove policy on recent macOS
    IB-7167
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- uninstall.sh
+++ uninstall.sh
@@ -20,10 +20,7 @@ function remove_all {
         /Library/Application\ Support/Mozilla/Extensions/\{ec8030f7-c20a-464f-9b0e-13a3a9e97384\}/\{443830f0-1fff-4f9a-aa1e-444bafbc7319\}.xpi \
         /Library/Application\ Support/Mozilla/Extensions/\{ec8030f7-c20a-464f-9b0e-13a3a9e97384\}/\{02274e0c-d135-45f0-8a9c-32b35110e10d\}.xpi \
         /Applications/Utilities/TokenSigningSafariHelper.app
-    base_ver="10.15.99"
-    if [ $(echo -e $base_ver"\n"$(sw_vers -productVersion) | sort -V | tail -1) == "$base_ver" ]; then 
-        sudo profiles remove --identifier ee.ria.chrome-token-signing
-    fi
+    sudo profiles remove --identifier ee.ria.chrome-token-signing
     osascript -e 'tell application "System Events" to delete login item "TokenSigningSafariHelper"'
     PLIST=/Library/Preferences/org.mozilla.firefox.plist
     sudo defaults write ${PLIST} ExtensionSettings -dict-add "'{02274e0c-d135-45f0-8a9c-32b35110e10d}'" "{ 'installation_mode' = 'blocked'; }"
@@ -37,15 +34,14 @@ function remove_all {
     echo 'Cleaning up'
     sudo pkgutil --forget ee.ria.open-eid
     sudo pkgutil --forget ee.ria.ID-updater
+    sudo pkgutil --forget ee.ria.esteid-tokend
+    sudo pkgutil --forget ee.ria.esteid-ctk-tokend
+    sudo pkgutil --forget ee.ria.firefox-pkcs11-loader
     sudo pkgutil --forget ee.ria.chrome-token-signing
     sudo pkgutil --forget ee.ria.token-signing-chrome
     sudo pkgutil --forget ee.ria.token-signing-firefox
     sudo pkgutil --forget ee.ria.token-signing-chrome-policy
-    sudo pkgutil --forget ee.ria.chrome-token-signing-policy
     sudo pkgutil --forget ee.ria.safari-token-signing
-    sudo pkgutil --forget ee.ria.firefox-pkcs11-loader
-    sudo pkgutil --forget ee.ria.esteid-tokend
-    sudo pkgutil --forget ee.ria.esteid-ctk-tokend
     sudo pkgutil --forget org.opensc-project.mac
     sudo rm -rf /Library/Preferences/ee.ria.*
 
